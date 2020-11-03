2d32e3230a58dce96a5f7ff3d67c1d35b0537f48
dingyl <yunlong.ding@transsnet.com>
2020-11-03 11:10:08 +0800

    first Commit And two answer

---
+++ src/com/welson/Main.java
@@ -0,0 +1,9 @@
+package com.welson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Q941 q941 = new Q941();
+        System.out.println(q941.validMountainArray(new int[]{2,1}));
+    }
+}
