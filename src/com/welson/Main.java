ec816fde6b048c3f998c7f254b8cf2f878520a60
dingyl <yunlong.ding@transsnet.com>
2020-11-30 19:59:13 +0800

    commot 13

--- src/com/welson/Main.java
+++ src/com/welson/Main.java
@@ -3,7 +3,7 @@ package com.welson;
 public class Main {
 
     public static void main(String[] args) {
-        Q941 q941 = new Q941();
-        System.out.println(q941.validMountainArray(new int[]{2,1}));
+        Q3 q3 = new Q3();
+        System.out.println(q3.lengthOfLongestSubstring("bbtablud"));
     }
 }
---
+++ src/com/welson/Q452.java
@@ -0,0 +1,11 @@
+package com.welson;
+
+public class Q452 {
+
+    /*public int findMinArrowShots(int[][] points) {
+        for (int i = 0; i < points.length; i++) {
+            
+        }
+    }*/
+
+}
