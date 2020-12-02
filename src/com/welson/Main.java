4b07ffacd97298187b6837b8d5acbf9f62d98c90
welson <dingyunlong1996@hotmail.com>
2020-12-02 22:34:31 +0800

    add Q4

--- src/com/welson/Main.java
+++ src/com/welson/Main.java
@@ -3,7 +3,9 @@ package com.welson;
 public class Main {
 
     public static void main(String[] args) {
-        Q3 q3 = new Q3();
-        System.out.println(q3.lengthOfLongestSubstring("bbtablud"));
+        Q4 q4 = new Q4();
+        int[] nums1 = new int[]{2};
+        int[] nums2 = new int[]{};
+        System.out.println(q4.findMedianSortedArrays(nums1,nums2));
     }
 }
