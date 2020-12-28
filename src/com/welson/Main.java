f5da2ed2f63084b22375104103e4b7928069d267
dingyl <yunlong.ding@transsnet.com>
2020-12-28 19:45:39 +0800

    commit 15

--- src/com/welson/Main.java
+++ src/com/welson/Main.java
@@ -3,7 +3,8 @@ package com.welson;
 public class Main {
 
     public static void main(String[] args) {
-        Q13 q4 = new Q13();
-        System.out.println(q4.romanToInt("IV"));
+        Q15 q4 = new Q15();
+        int[] arr = new int[]{0,0,0,0,0,0};
+        System.out.println(q4.threeSum(arr));
     }
 }
