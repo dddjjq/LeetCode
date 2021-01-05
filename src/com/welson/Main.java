28f09c0e116714c021cd90cbd6df163733a4423e
dingyl <yunlong.ding@transsnet.com>
2021-01-05 15:27:20 +0800

    commit 17 18

--- src/com/welson/Main.java
+++ src/com/welson/Main.java
@@ -3,8 +3,8 @@ package com.welson;
 public class Main {
 
     public static void main(String[] args) {
-        Q16 q4 = new Q16();
-        int[] arr = new int[]{0,0,0,0,0,2,1};
-        System.out.println(q4.threeSumClosest(arr,1));
+        Q18 q4 = new Q18();
+        int[] arr = new int[]{-3,-1,0,2,4,5};
+        System.out.println(q4.fourSum(arr,0));
     }
 }
---
+++ src/com/welson/Q18.java
@@ -0,0 +1,37 @@
+package com.welson;
+
+import java.util.*;
+
+public class Q18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int length = nums.length;
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < length; i++) {
+            for (int j = length - 1; j >= 0; j--) {
+                int sum = nums[i] + nums[j];
+                int start1 = i + 1;
+                int end1 = j - 1;
+                while (start1 < end1) {
+                    sum += nums[start1] + nums[end1];
+                    if (sum == target) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[start1]);
+                        list.add(nums[end1]);
+                        set.add(list);
+                        start1++;
+                        end1--;
+                    } else if (sum < target) {
+                        start1++;
+                    } else {
+                        end1--;
+                    }
+                    sum = nums[i] + nums[j];
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
